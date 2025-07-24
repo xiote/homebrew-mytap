e8868bce1aaf64f1f13df552e5a3e3c73f019e18
xiote <xiote@nate.com>
2025-07-24 17:00:57 +0900

    Update opencv@4.11.0.rb

--- Formula/opencv@4.11.0.rb
+++ Formula/opencv@4.11.0.rb
@@ -1,205 +1,42 @@
 class OpencvAT4110 < Formula
-  desc "Open source computer vision library"
-  homepage "https://opencv.org/"
-  license "Apache-2.0"
-  revision 1
+  desc      "Open source computer vision library"
+  homepage  "https://opencv.org/"
+  # 4.11.0 공식 소스 tarball
+  url       "https://github.com/opencv/opencv/archive/4.11.0.tar.gz"
+  # SHA-256 값은 upstream tarball 기준 (Termux 빌드 스크립트에서 확인)   [oai_citation:1‡GitHub](https://raw.githubusercontent.com/termux/termux-packages/67838d71d5/x11-packages/opencv/build.sh)
+  sha256    "9a7c11f924eff5f8d8070e297b322ee68b9227e003fd600d4b8122198091665f"
+  license   "Apache-2.0"
 
-  stable do
-    url "https://github.com/opencv/opencv/archive/refs/tags/4.11.0.tar.gz"
-    sha256 "9a7c11f924eff5f8d8070e297b322ee68b9227e003fd600d4b8122198091665f"
-
-    resource "contrib" do
-      url "https://github.com/opencv/opencv_contrib/archive/refs/tags/4.11.0.tar.gz"
-      sha256 "2dfc5957201de2aa785064711125af6abb2e80a64e2dc246aca4119b19687041"
-
-      livecheck do
-        formula :parent
-      end
-    end
-  end
-
-  livecheck do
-    url :stable
-    regex(/^v?(\d+(?:\.\d+)+)$/i)
-  end
-
-  bottle do
-    sha256 arm64_sonoma:  "14e468ccb54da0d671e31730284f933508d5195a208aca7a9b08541ada70c32a"
-    sha256 arm64_ventura: "b70e530ca8d0aeb8e6bc91a13c5d1fffd114fce6807f517b8d87c9375cf18b05"
-    sha256 sonoma:        "b2ff125058f1b6c8b409c3510d833fbd22ee9a025663f8acb91bab12d03aec5e"
-    sha256 ventura:       "690e423d0e51cca672a27d570f452ed7d8d867ba43d808ac7fb5156768d5d6c5"
-    sha256 x86_64_linux:  "f91667972adca2f3e7e286ab0a8cd1623b7b14954f01d233a7efb8143cf66e85"
-  end
-
-  head do
-    url "https://github.com/opencv/opencv.git", branch: "master"
-
-    resource "contrib" do
-      url "https://github.com/opencv/opencv_contrib.git", branch: "master"
-    end
-  end
-
-  depends_on "cmake" => :build
-  depends_on "pkgconf" => :build
-  depends_on "python-setuptools" => :build
-  depends_on "abseil"
-  depends_on "ceres-solver"
+  # ---------- 필수 의존성 ----------
+  depends_on "cmake"      => :build   # 빌드 도구
+  depends_on "pkg-config" => :build   # 패키지 메타
   depends_on "eigen"
   depends_on "ffmpeg"
-  depends_on "freetype"
-  depends_on "gflags"
-  depends_on "glog"
-  depends_on "harfbuzz"
-  depends_on "jpeg-turbo"
-  depends_on "jsoncpp"
-  depends_on "libpng"
-  depends_on "libtiff"
-  depends_on "numpy"
   depends_on "openblas"
-  depends_on "openexr"
-  depends_on "openjpeg"
-  depends_on "openvino"
-  depends_on "protobuf"
-  depends_on "python@3.13"
-  depends_on "tbb"
-  depends_on "tesseract"
-  depends_on "vtk"
-  depends_on "webp"
+  # …(현재 opencv.rb 의존성을 그대로 복사해도 됨)
 
-  uses_from_macos "zlib"
-
-  on_macos do
-    depends_on "glew"
-    depends_on "imath"
-    depends_on "libarchive"
-  end
-
-  on_linux do
-    depends_on "cairo"
-    depends_on "gdk-pixbuf"
-    depends_on "glib"
-    depends_on "gtk+3"
-  end
-
-  def python3
-    "python3.13"
-  end
-
+  # ---------- 설치 ----------
   def install
-    resource("contrib").stage buildpath/"opencv_contrib"
-
-    # Avoid Accelerate.framework
-    ENV["OpenBLAS_HOME"] = Formula["openblas"].opt_prefix
-
-    # Reset PYTHONPATH, workaround for https://github.com/Homebrew/homebrew-science/pull/4885
-    ENV.delete("PYTHONPATH")
-
-    # Remove bundled libraries to make sure formula dependencies are used
-    libdirs = %w[ffmpeg libjasper libjpeg libjpeg-turbo libpng libtiff libwebp openexr openjpeg protobuf tbb zlib]
-    libdirs.each { |l| rm_r(buildpath/"3rdparty"/l) }
-
-    args = %W[
-      -DCMAKE_CXX_STANDARD=17
-      -DCMAKE_OSX_DEPLOYMENT_TARGET=
-      -DBUILD_JASPER=OFF
-      -DBUILD_JPEG=OFF
-      -DBUILD_OPENEXR=OFF
-      -DBUILD_OPENJPEG=OFF
-      -DBUILD_PERF_TESTS=OFF
-      -DBUILD_PNG=OFF
-      -DBUILD_PROTOBUF=OFF
-      -DBUILD_TBB=OFF
+    args = std_cmake_args + %W[
+      -DCMAKE_OSX_DEPLOYMENT_TARGET=#{MacOS.version}
+      -DBUILD_JAVA=OFF     # 불필요한 랩퍼 비활성화
       -DBUILD_TESTS=OFF
-      -DBUILD_TIFF=OFF
-      -DBUILD_WEBP=OFF
-      -DBUILD_ZLIB=OFF
-      -DBUILD_opencv_hdf=OFF
-      -DBUILD_opencv_java=OFF
-      -DBUILD_opencv_text=ON
-      -DOPENCV_ENABLE_NONFREE=ON
-      -DOPENCV_EXTRA_MODULES_PATH=#{buildpath}/opencv_contrib/modules
-      -DOPENCV_GENERATE_PKGCONFIG=ON
-      -DPROTOBUF_UPDATE_FILES=ON
-      -DWITH_1394=OFF
-      -DWITH_CUDA=OFF
-      -DWITH_EIGEN=ON
-      -DWITH_FFMPEG=ON
-      -DWITH_GPHOTO2=OFF
-      -DWITH_GSTREAMER=OFF
-      -DWITH_JASPER=OFF
-      -DWITH_OPENEXR=ON
-      -DWITH_OPENGL=OFF
-      -DWITH_OPENVINO=ON
-      -DWITH_QT=OFF
-      -DWITH_TBB=ON
-      -DWITH_VTK=ON
-      -DBUILD_opencv_python2=OFF
-      -DBUILD_opencv_python3=ON
-      -DPYTHON3_EXECUTABLE=#{which(python3)}
+      -DBUILD_PERF_TESTS=OFF
     ]
-
-    args += [
-      "-DCMAKE_FIND_PACKAGE_PREFER_CONFIG=ON", # https://github.com/protocolbuffers/protobuf/issues/12292
-      "-Dprotobuf_MODULE_COMPATIBLE=ON", # https://github.com/protocolbuffers/protobuf/issues/1931
-    ]
-
-    args += if OS.mac?
-      # Requires closed-source, pre-built Orbbec SDK on macOS
-      ["-DWITH_OBSENSOR=OFF"]
-    else
-      # Disable precompiled headers and force opencv to use brewed libraries on Linux
-      %W[
-        -DENABLE_PRECOMPILED_HEADERS=OFF
-        -DJPEG_LIBRARY=#{Formula["jpeg-turbo"].opt_lib}/libjpeg.so
-        -DOpenBLAS_LIB=#{Formula["openblas"].opt_lib}/libopenblas.so
-        -DOPENEXR_ILMIMF_LIBRARY=#{Formula["openexr"].opt_lib}/libIlmImf.so
-        -DOPENEXR_ILMTHREAD_LIBRARY=#{Formula["openexr"].opt_lib}/libIlmThread.so
-        -DPNG_LIBRARY=#{Formula["libpng"].opt_lib}/libpng.so
-        -DPROTOBUF_LIBRARY=#{Formula["protobuf"].opt_lib}/libprotobuf.so
-        -DTIFF_LIBRARY=#{Formula["libtiff"].opt_lib}/libtiff.so
-        -DWITH_V4L=OFF
-        -DZLIB_LIBRARY=#{Formula["zlib"].opt_lib}/libz.so
-      ]
-    end
-
-    # Ref: https://github.com/opencv/opencv/wiki/CPU-optimizations-build-options
-    ENV.runtime_cpu_detection
-    if Hardware::CPU.intel? && build.bottle?
-      cpu_baseline = if OS.mac? && MacOS.version.requires_sse42?
-        "SSE4_2"
-      else
-        "SSSE3"
-      end
-      args += %W[-DCPU_BASELINE=#{cpu_baseline} -DCPU_BASELINE_REQUIRE=#{cpu_baseline}]
-    end
-
-    system "cmake", "-S", ".", "-B", "build_shared", *args, *std_cmake_args
-    inreplace "build_shared/modules/core/version_string.inc", "#{Superenv.shims_path}/", ""
-    system "cmake", "--build", "build_shared"
-    system "cmake", "--install", "build_shared"
-
-    system "cmake", "-S", ".", "-B", "build_static", *args, *std_cmake_args, "-DBUILD_SHARED_LIBS=OFF"
-    inreplace "build_static/modules/core/version_string.inc", "#{Superenv.shims_path}/", ""
-    system "cmake", "--build", "build_static"
-    lib.install buildpath.glob("build_static/{lib,3rdparty/**}/*.a")
-
-    # Prevent dependents from using fragile Cellar paths
-    inreplace lib/"pkgconfig/opencv#{version.major}.pc", prefix, opt_prefix
+    system "cmake", ".", *args
+    system "make", "-j#{ENV.make_jobs}"
+    system "make", "install"
   end
 
+  # ---------- 간단 테스트 ----------
   test do
-    (testpath/"test.cpp").write <<~CPP
-      #include <opencv2/opencv.hpp>
+    (testpath/"version.cpp").write <<~EOS
+      #include <opencv2/core/core.hpp>
       #include <iostream>
-      int main() {
-        std::cout << CV_VERSION << std::endl;
-        return 0;
-      }
-    CPP
-    system ENV.cxx, "-std=c++17", "test.cpp", "-I#{include}/opencv4", "-o", "test"
-    assert_equal version.to_s, shell_output("./test").strip
-
-    output = shell_output("#{python3} -c 'import cv2; print(cv2.__version__)'")
-    assert_equal version.to_s, output.chomp
+      int main() { std::cout << CV_VERSION; return 0; }
+    EOS
+    system ENV.cxx, "version.cpp", "-I#{include}/opencv4",
+                    "-L#{lib}", "-lopencv_core", "-std=c++17", "-o", "ver"
+    assert_match "4.11.0", shell_output("./ver")
   end
 end
